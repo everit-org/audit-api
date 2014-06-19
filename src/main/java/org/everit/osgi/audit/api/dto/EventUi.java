14995111c843f3eaa188cc367c67f3519da0f426
bence.eros <bence.eros@everit.biz>
2014-06-19 16:50:33 +0200

    added builder class for EventUi

--- src/main/java/org/everit/osgi/audit/api/dto/EventUi.java
+++ src/main/java/org/everit/osgi/audit/api/dto/EventUi.java
@@ -16,12 +16,85 @@
  */
 package org.everit.osgi.audit.api.dto;
 
+import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class EventUi {
+
+    public static class Builder {
+
+        private long eventId;
+
+        private String typeName;
+
+        private String appName;
+
+        private Calendar saveTimestamp;
+
+        private final Map<String, EventData> eventData = new HashMap<>();
+
+        public Builder appName(final String appName) {
+            this.appName = appName;
+            return this;
+        }
+
+        public Builder binaryData(final String name, final byte[] data) {
+            eventData.put(name, new EventData(name, data));
+            return this;
+        }
+
+        public EventUi build() {
+            return new EventUi(eventId, typeName, appName, saveTimestamp, eventData);
+        }
+
+        public Builder eventId(final long eventId) {
+            this.eventId = eventId;
+            return this;
+        }
+
+        public Builder numberData(final String name, final double number) {
+            eventData.put(name, new EventData(name, number));
+            return this;
+        }
+
+        public Builder saveTimestamp(final Calendar saveTimestamp) {
+            this.saveTimestamp = saveTimestamp;
+            return this;
+        }
+
+        public Builder saveTimestamp(final Timestamp timestamp) {
+            Calendar cal = new GregorianCalendar();
+            cal.setTimeInMillis(timestamp.getTime());
+            return saveTimestamp(cal);
+        }
+
+        public Builder stringData(final String name, final String data) {
+            eventData.put(name, new EventData(name, data));
+            return this;
+        }
+
+        public Builder textData(final String name, final String data) {
+            eventData.put(name, new EventData(name, false, data));
+            return this;
+        }
+
+        public Builder timestampData(final String name, final Calendar data) {
+            eventData.put(name, new EventData(name, data));
+            return this;
+        }
+
+        public Builder typeName(final String typeName) {
+            this.typeName = typeName;
+            return this;
+        }
+
+    }
+
     /**
      * Id of the event.
      */
@@ -41,17 +114,20 @@ public class EventUi {
     /**
      * Map containing the event data.
      */
-    private Map<String, EventData> eventData;
+    private final Map<String, EventData> eventData;
 
-    public EventUi(final long id, final String name, final String applicationName, final Calendar saveTimeStamp) {
-        this(id, name, applicationName, saveTimeStamp.getTime());
+    public EventUi(final long id, final String name, final String applicationName, final Calendar saveTimeStamp,
+            final Map<String, EventData> eventData) {
+        this(id, name, applicationName, saveTimeStamp.getTime(), eventData);
     }
 
-    public EventUi(final Long id, final String name, final String applicationName, final Date saveTimeStamp) {
+    public EventUi(final Long id, final String name, final String applicationName, final Date saveTimeStamp,
+            final Map<String, EventData> eventData) {
         this.id = id;
         this.name = Objects.requireNonNull(name, "name cannot be null");
         this.applicationName = Objects.requireNonNull(applicationName, "applicationName cannot be null");
         this.saveTimeStamp = new Date(saveTimeStamp.getTime());
+        this.eventData = Objects.requireNonNull(eventData, "eventData cannot be null");
     }
 
     public String getApplicationName() {
